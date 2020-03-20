e8d5aa8e182c34277c7e908bd40fef3ea469507c
Sean Pettit <s.p@mac.com>
2020-03-20 16:32:49 +0000

    fixing bug with inline images not showing up

--- src/templates/legs.js
+++ src/templates/legs.js
@@ -3,10 +3,12 @@ import { graphql } from "gatsby"
 
 export default ({ data }) => {
   const post = data.markdownRemark
+  // const newHtml = post.html.replace(/img src="/g, 'img src="https://caribbean-odyssey.netlify.com/')
+  const newHtml = post.html.replace(/img src="/g, 'img src="'+window.location.origin+'/')
   return (
       <div>
         <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div dangerouslySetInnerHTML={{ __html: newHtml }} />
       </div>
   )
 }
